ce765f8c572b4d009df40a70a2f7f7c5129f8081
jvcorado <jhonisilva545@gmail.com>
2024-05-28 10:32:18 -0300

    tipando os produtos listados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,35 @@
-export default function Home() {
+import Image from "next/image";
+import { Key } from "react";
+import { ProductType } from "../types/productType";
+
+async function getProducts() {
+  const response = await fetch("https://fakestoreapi.com/products/");
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return response.json();
+}
+
+export default async function Home() {
+  const products = await getProducts();
+
+  console.log(products);
+
   return (
     <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
-      <div className=" h-[400px] bg-slate-500">card</div>
-      <div className=" h-[400px] bg-slate-500">card</div>
-      <div className=" h-[400px] bg-slate-500">card</div>
-      <div className=" h-[400px] bg-slate-500">card</div>
-      <div className=" h-[400px] bg-slate-500">card</div>
-      <div className=" h-[400px] bg-slate-500">card</div>
+      {products.map((item: ProductType) => (
+        <div key={item.id} className="h-[400px] bg-slate-500">
+          <Image
+            src={item.image}
+            alt={item.title}
+            width={100}
+            height={100}
+          ></Image>
+          {item.title}
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/types/productType.ts
@@ -0,0 +1,9 @@
+export type ProductType = {
+    id: number;
+    title: string;
+    price: number | null;
+    description: string | null;
+    image: string;
+    category: string;
+
+}
