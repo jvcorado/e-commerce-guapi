1e997389061f9e094533ba9c835b947e4c88ac32
jvcorado <jhonisilva545@gmail.com>
2024-05-28 09:42:35 -0300

    Adicionando NavBar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import NavBar from "./components/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavBar />
+        <main className="container mx-auto flex items-center justify-center bg-slate-700 h-screen">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
